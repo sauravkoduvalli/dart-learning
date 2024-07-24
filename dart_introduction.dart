0351482b5e2c21b9651d4e400f0ec73c7ae3ebb5
Saurav K <sauravkoduvalli@gmail.com>
2024-07-24 21:53:40 +0530

    started from basics

---
+++ dart_introduction.dart
@@ -0,0 +1,23 @@
+/* --- Dart Programming language --- */
+/*
+  - Dart is an open-source, object oriented, programming language developed by google in 2021.
+  - It supports application development on both the client and server side. But, widely used for the development of Android apps, iOS apps, IoT(Internet of Things), and web applications using the Flutter Framework.
+  - Dart is a compiled language, which means cannot directly execute the code, the code will be compiled to machine code for the execution.
+  - Dart is a strongly typed programming language, meaning each data value in the code should have a type and must be known when the code is compiled.
+*/
+
+
+/* --- Why use Dart? --- */
+/*
+  - Fast & Smooth: Dart compiles to native code for speedy performance, ideal for mobile apps.
+  - Easy to Learn: Similar to familiar languages like Java or Javascript, making it approachable for new developers.
+  - Flutter Power: Dart is the heart of Flutter, a popular framework for building beautiful and functional mobile apps.
+  - One Code, Many Places: Develop for mobile, web, and even desktop with a single codebase (primarily with Flutter).
+*/
+
+/* --- Dart Compilators --- */
+/*
+  1) Just-In-Time (JIT):
+  2) Ahead-Of-Time (AOT):
+  3) Dart2js: 
+*/
