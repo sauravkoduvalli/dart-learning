91e553599ef038e97dd5861543ae29468d088da6
Saurav K <sauravkoduvalli@gmail.com>
2024-07-28 21:56:41 +0530

    arithmetic operation theory added

---
+++ basics/operators.dart
@@ -0,0 +1,16 @@
+/* ---Operators --- */
+
+// Operators are special symbols which is used to person certian mathermatical computation, assignment, locagical operation on operands.
+
+/*
+Types of operators in dart are:
+  1) Arithmetic: Operators which is used to perform arithmetic operations on operands. [addition(+), subtraction(-), multiplication(*), division(/), division(~/), modulus(%), undary minus(-expr), ]
+  2) Assignment: Operators used to perform assign values to the operands. [equal to(=), Assign if null(??=)]
+  3) Relational: Operators used to check the ralation between to operands. [geater than(>), less than(<), greater than or equal to (>=), less than or equal to(<=), equal to(==), not equal to(!=)].
+  4) Type Test: Operators used to perform comparison on operands. [is(is), is not(is!)]
+  5) Bitwise: Operators used to perform bitwise operation on the operands. [bitwise AND(&), bitwise OR(|), bitwise XOR(^), bitwise NOT(~), left shift(<<), right shift(>>), unsigned shift right(>>>)]
+  6) Logical: Operators used to logically combine two or more conditions of the operands. [logical AND(&&), logical OR(||), logical NOT(!)]
+  7) Conditional: Operators used to perform operations based on value of certian condition or expression. [ternary(condition? exp1: exp2), null oalescing(??)] 
+  8) Cascade Notation: Operators allows you to perform a sequence of operation on the same element or allows you to perform multiple methods on the same object. [cascading(..), null shorting cascade(..?)]
+*/
+
