008cf8db5aee0a5ee02c8897346f5e726f6c5ce2
Saurav K <sauravkoduvalli@gmail.com>
2024-08-31 23:03:24 +0530

    late mutability comments addded

--- basics/variables/late.dart
+++ basics/variables/late.dart
@@ -17,13 +17,12 @@
     - if a variable's initialization involves expensive computations, use late keyword to defer the computation until the value is actually needed.
 
   4) Mutability and reassignment:-
-    - A varibale can be reassigned if it is a var or a mutable type. However a 'late  final'  varible  is assigned, it cannot be reasigned.
+    - A varibale can be reassigned if it is a var or a mutable type. However once a 'late final' varible is assigned, it cannot be reasigned.
   
   5) Error handling:-
     - If a vaiable is accessed before it is initialized, the program will throw a runtime error (LateInitializationError), indicating that the varibale is not assigned yet
     
  */
-
 
 class MyClass {
   // late variable declared
@@ -35,23 +34,42 @@ class MyClass {
   }
 
   // method for printing that value
-  void printDescription(){
+  void printDescription() {
     print(description);
   }
 }
 
+class ComputationClass {
+  // late final String name = getName();
+  late var name = getName();
 
+  String getName() {
+    print('Fetching name...');
+    return 'Saurav Koduvalli';
+  }
+}
 
 void main() {
   // creating a instance/object of 'MyClass'
   final myObject = MyClass();
 
-  // calling initialiseDescription() method, will assign a value to 'description' variable
-  myObject.initialiseDescription(); // try commenting this method and run the code, will throw "LateInitializationError" error.
+  // Will assign a value to 'description' variable
+  myObject
+      .initialiseDescription(); // try commenting this method and run the code, will throw "LateInitializationError" error.
 
-  // calling printDescription() method, will print the assinged value.
+  // Will print the assinged value.
   myObject.printDescription();
 
+  // creating instance for ComputationClass
+  final computationObject = ComputationClass();
+
+  // At this point name is not yet initialized
+  print("before accessing name");
+
+  // Now the name is accessed, triggering the initialization
+  print(computationObject.name);
+  computationObject.name = "Saurav K"; // re-assigned since this variable is declared as var in its calss. Use 'late final VaiableType' instead of 'late var', will not allow re-assigning.
+  print(computationObject.name);
 }
 
 /// Success Case: The 'late variable is assigned with initialiseDescription() method, so that the variable can be used.'
