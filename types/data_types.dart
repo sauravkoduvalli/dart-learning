43ae51f9b67f02f1eceaf81506058140ae859721
Saurav K <sauravkoduvalli@gmail.com>
2024-09-02 14:51:55 +0530

    folder structure change

---
+++ types/data_types.dart
@@ -0,0 +1,16 @@
+/* --- Data Types --- */
+
+/* 
+  - Data Type scpecifies what type of data/value will be stored in a variable. Each variable has its own data type.
+  - Example for some built-in data types are,
+    1) Strings (String) - represents sequesnce of charecters.
+    2) Numbers (int, double) - represents integer values
+    3) Booleans (bool) - represnet true and false
+    4) Records ((value1, value2)) - 
+    5) Lists (List are also know as Arrays) - unordered collection of same data type items.
+    6) Sets (Set) - ordered collection of unique data items
+    7) Maps (Map) - an object that represnet values as key-value pair.
+    8) Runes (Runes; often replaced by charecters API) - represents the unicode code points of string.
+    9) Symbols (Symbol) - represents an operator or identifier in dart program
+    10) The value null (Null) - represnets the absence of value. which is empty or nothing.
+*/
