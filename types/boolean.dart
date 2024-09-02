83d2dc38475b5dc7a1da0e130674e8b782340667
Saurav K <sauravkoduvalli@gmail.com>
2024-09-02 17:30:50 +0530

    basic data types added

---
+++ types/boolean.dart
@@ -0,0 +1,61 @@
+/* --- Boolean --- */
+
+/*
+  - Dart uses 'bool' to represent boolean values.
+  - Type bool type has two constants True and False.
+
+  In practice, boolean values often use in control statements such as if else, while do-while and for.
+  
+  Logical Operators
+  Logical AND (&&) - Accepts two boolean values and return TRUE if both of the are TRUE. otherview return FALSE
+  Logical OR (||) - Accepts two boolean values and return FALSE if both values are FALSE, otherwise return TRUE.
+  Logical NOT (!) - Accepts a boolean value and negate it.
+
+  Short-circut Evaluation:-
+    - Both logical And and OR operators are short-circuted.
+    - if the first value is FALSE, then logical AND operator will be FALSE regardless of the socond value. Therefore, Logical AND operator will not evaluate the second value if the first value is FALSE.
+    - if the first value is TRUE, then the logical OR operatot will return TRUE regardless of the second value. Therefore, Logical OR operator will not evaluate the second value if the first value is TRUE.
+    
+*/
+
+
+
+void main() {
+  /// declares a variable with the bool type and initialize its value to true
+  bool isActive = true;
+  print(isActive); // true
+
+  // program which will show how logical AND will work
+  bool isUsernameCorrect = true;
+  bool isPasswordCorrect = true;
+
+  bool isAuthenticated = isUsernameCorrect && isPasswordCorrect;
+  print('Authentication is $isAuthenticated');
+
+  // program which will show how logical OR will work
+  bool passwordLengthMatch = false;
+  bool passwordContainNumbers = false;
+
+  bool isValidPassword = passwordContainNumbers || passwordLengthMatch;
+  print("Password validation is $isValidPassword");
+
+  /// program will show how logical NOT works
+  bool isVegetarien = false;
+  print(!isVegetarien); // TRUE
+
+  /// program will show how short-circut will work in logical AND
+  bool value1 = false;
+  bool value2 = true;
+
+  bool result = value1 && value2; // as you can see the value of 'value2' variable is not evaluating by the dart compiler.
+  print('Short-circute AND: $result');
+
+  /// program will show how short-circut will work in Logical OR
+  bool isHuman = false;
+  bool fromEarth = true;
+
+  bool isAlien = fromEarth || isHuman ; // as you can see the value of 'fromEarth variable is not evaluating by the dart compiler.
+  print("Are you an Alien: $isAlien"); 
+
+
+}
--- types/numbers.dart
+++ types/numbers.dart
@@ -1,9 +1,9 @@
 /* --- Numbers --- */
 
 /*
-  - Helps to store numeric values in a variable.
+  - 'int' type helps to store numeric values in a variable.
   - Unsigned numbers are always a positive number. Numbers can be either negative or positive.
-  - @ subset are int and double. 
+  - subset are 'int' and 'double'. 
   - Use 'int'(without decimal) for declaring integers or whole numbers.
   - Use 'double'(with decimal) for fractional numbers or floating-point numbers,
 */
@@ -11,13 +11,28 @@
 void main() {
   final int date = 27;
   final double doubleValue = 270.998;
-  // create raw string in dart. Special characters won’t work here. You must write r after equal sign.
+
+  /// converting an string into integer, use `int.parse(expresison)`.
+  int quantity = 5;
+  String amount = "100";
+  int total =  quantity * int.parse(amount);
+  print('Total is: $total'); // 500
+
+  /// converting 'quantity' int varibale to double
+  var inToDouble = quantity.toDouble();
+  print(inToDouble.runtimeType); // int
+
+  /// converting 'amount' string varibale to double
+  var stringToDouble = double.parse(amount);
+  print(stringToDouble.runtimeType); // double
+
+  /// create raw string in dart. Special characters won’t work here. You must write r after equal sign.
   String withoutRawString = "The value of price is \t $date"; // regular String
-  String withRawString = r"The value of price is \t $date"; // raw String
   print(withoutRawString);
+  String withRawString = r"The value of price is \t $date"; // raw String
   print(withRawString);
 
-  // basic properties
+  /// basic properties
   print('isEven: ${date.isEven}'); // check given value is even, if Yes return TRUE, else FALSE.
   print('isOdd: ${date.isOdd}'); // check given value is even, if Yes return TRUE, else FALSE.
   print('isFinite: ${date.isFinite}'); // If the given number is finite, then it returns TRUE.
@@ -25,7 +40,7 @@ void main() {
   print('isNegative: ${date.isNegative}'); // If the number is negative then it returns TRUE.
   print('sign: ${date.sign}'); // It returns -1, 0, or 1 depending upon the sign of the given number.
   
-  // basic methods
+  /// basic methods
   print('toInt: ${doubleValue.toInt()}'); // It will convert double value to int.
   print('abs: ${doubleValue.abs()}'); // It gives the absolute value of the given number.
   print('ceil: ${doubleValue.ceil()}'); // It gives the ceiling value of the given number.
