3e1e494c5e0c3d24219d03565abe9e4e3b29cf94
Saurav K <sauravkoduvalli@gmail.com>
2024-11-10 20:59:16 +0530

    added more content

--- advanced concepts/mixins.dart
+++ advanced concepts/mixins.dart
@@ -5,13 +5,24 @@
   - They provide a way to add additional functionality to a class without creating a deep inheritance chain.
   - Use the 'mixin' keyword to define a mixin. Unlike a class, a mixin cannot be instantiated and it cannot have a constructor.
 
-  How Mixins Work:
+  How Mixins Work
   ---------------
   - Declaration: A mixin is defined using the mixin keyword.
   - Usage: You can apply a mixin to a class using the with keyword.
   - Functionality: A mixin can contain methods, properties, and even its own superclass.
 
-  
+  Key Points
+  -----------
+  - Multiple Mixins: A class can apply multiple mixins. 
+  - Order of Mixins: The order of mixins matters, as it determines the method resolution order.
+  - No Constructor: Mixins cannot have constructors.   
+  - No Inheritance: Mixins cannot inherit from other classes.
+
+  Benefits of using mixins
+  ------------------------
+  - Code Reusability: Share code between multiple classes without creating complex inheritance hierarchies.   
+  - Flexibility: Easily add new functionality to existing classes.
+  - Avoids Diamond Problem: Mixins help prevent the diamond problem, a common issue in multiple inheritance
 */
 
 abstract class Model {}
